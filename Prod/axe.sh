887608fc750b11d18b915c433ee5856535ffeb49
axeprpr <axeprpr@gmail.com>
2017-01-06 00:08:43 +0800

    删除密码信息

--- Prod/axe.sh
+++ Prod/axe.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 [ $1 ]|| exit 1
-if [ $1 == "vps" ];then
-    sshpass -p "XW2fif5#" ssh root@axeprpr.com
-elif [ $1 == "troll" ];then
-    sshpass -p "XW2fif5#" ssh axe@203.110.138.12
-else
-    sshpass -p "astutepass" ssh astute@192.222.1.${1}
-fi
+sshpass -p "astutepass" ssh astute@192.222.1.${1}
 
 
 
