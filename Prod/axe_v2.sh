1891acd086e388c6e10e520b77b53278561ad366
axeprpr <axeprpr@gmail.com>
2017-02-21 19:22:35 +0800

    bug修改

--- Prod/axe_v2.sh
+++ Prod/axe_v2.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/expect
-echo > /Users/axe/.ssh/known_hosts
+spawn echo > /Users/axe/.ssh/known_hosts
 set timeout 30
 spawn ssh -p [lindex $argv 0] [lindex $argv 1]@[lindex $argv 2]
 expect {
