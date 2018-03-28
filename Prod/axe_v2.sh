9e565555d8cfbcb02f1efb6852e10abfcda97266
axeprpr <axeprpr@gmail.com>
2018-03-28 17:13:21 +0800

    git快捷

--- Prod/axe_v2.sh
+++ Prod/axe_v2.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/expect
-spawn echo > /Users/axe/.ssh/known_hosts
+spawn rm /Users/axe/.ssh/known_hosts
 set timeout 30
 spawn ssh -p [lindex $argv 0] [lindex $argv 1]@[lindex $argv 2]
 expect {
