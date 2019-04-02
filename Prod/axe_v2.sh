41dd12b3a6842a29cb2bcd89543ff23cac515aa4
Your Name <you@example.com>
2019-04-02 19:05:08 +0800

    xx

--- Prod/axe_v2.sh
+++ Prod/axe_v2.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/expect
-spawn rm /Users/axe/.ssh/known_hosts
+spawn rm /home/axe/.ssh/known_hosts
 set timeout 30
 spawn ssh -p [lindex $argv 0] [lindex $argv 1]@[lindex $argv 2]
 expect {
