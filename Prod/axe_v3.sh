61596bb9ddc9401867def27e386be312d7beb6d7
xv.yayun <xv.yayun@astute-tec.com>
2019-04-15 12:18:04 +0800

    xxx

--- Prod/axe_v3.sh
+++ Prod/axe_v3.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 [ $1 ] || exit
-/home/axe/axepro/Helloworld/Prod/axe_v2.sh 22 root "192.222.1.${1}" 'donotuseroot!'
+if [ $1 -eq 1 ];then
+  ip=192.221.1.1
+elif [ `echo $1|awk '{print length($0)}'` -le 3 ];then
+  ip=192.222.1.$1
+else
+  ip=$1
+fi
+/Users/axe/WorkGit/pro/Helloworld/Prod/axe_v2.sh 22 root "$ip" 'donotuseroot!'
